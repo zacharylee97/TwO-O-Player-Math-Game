1ee86094f8b6371cacf3669c58d23f2abd69af44
Zachary Lee <zacharylhlee@gmail.com>
2018-11-05 21:16:57 +0000

    Adds function to check if player has lost

--- main.rb
+++ main.rb
@@ -20,6 +20,21 @@ while current_player.lives > 0 do
   turn.problem()
   turn.check(gets.chomp)
   puts "P1: #{player_1.lives}/3 vs P2: #{player_2.lives}/3"
+  puts "----- NEW TURN -----"
+
+  # Check if current_player has no lives left
+  if current_player.lives == 0
+     # Change current_player to next player
+    if next_player(current_player)
+      current_player = player_2
+    else
+      current_player = player_1
+    end
+    puts "#{current_player.name} wins with a score of #{current_player.lives}/3"
+    puts "----- GAME OVER -----"
+    puts "Good bye!"
+    break
+  end
 
   # Change current_player to next player
   if next_player(current_player)
