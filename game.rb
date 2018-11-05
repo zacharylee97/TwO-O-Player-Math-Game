031fd6468db905c8419074041cf7d12ccfdebd68
Zachary Lee <zacharylhlee@gmail.com>
2018-11-05 21:40:11 +0000

    Refactors code to create helper functions

--- game.rb
+++ game.rb
@@ -12,7 +12,7 @@ class Game
     @round = 1
   end
 
-  def next_player(current_player)
+  def next_player()
     if self.round % 2 == 0
         self.current_player = self.players[1]
     else
@@ -20,29 +20,39 @@ class Game
     end
   end
 
+  def new_turn(player)
+    puts "----- ROUND #{self.round} -----"
+      turn = Turn.new(player)
+      turn.problem()
+      turn.check(gets.chomp)
+  end
+
+  def game_over?(player)
+    if player.lives == 0
+      return true
+    end
+    return false
+  end
+
   def run()
     puts "----- NEW GAME ----"
     while current_player.lives > 0 do
       # Create new Turn for current_player
-      puts "----- NEW TURN -----"
-      turn = Turn.new(current_player)
-      turn.problem()
-      turn.check(gets.chomp)
+      new_turn(current_player)
       puts "P1: #{player_1.lives}/3 vs P2: #{player_2.lives}/3"
       self.round += 1
-
       # Check if current_player has no lives left
-      if current_player.lives == 0
-         # Change current_player to next player
-        next_player(current_player)
-        puts "#{current_player.name} wins with a score of #{current_player.lives}/3"
+      if game_over?(current_player)
+        # Next player is declared the winner
+        next_player()
+        winner = current_player
+        puts "#{winner.name} wins with a score of #{winner.lives}/3"
         puts "----- GAME OVER -----"
         puts "Good bye!"
         break
       end
-
       # Change current_player to next player
-      next_player(current_player)
+      next_player()
     end
   end
 end
