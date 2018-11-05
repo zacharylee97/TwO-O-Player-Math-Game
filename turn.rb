ddfc30869c7cc3d1bfe0e12fef0d78fee352806c
Zachary Lee <zacharylhlee@gmail.com>
2018-11-05 19:48:25 +0000

    Adds functions to Turn class to display problem and to check answer

--- turn.rb
+++ turn.rb
@@ -7,4 +7,18 @@ class Turn
     @question = Question.new()
     @answer = @question.answer
   end
+  # Print out problem for current player
+  def problem()
+    puts "#{self.player.name}: What does #{self.question.a} plus #{self.question.b} equal?"
+    print "> "
+  end
+  # Check if input from player matches correct answer to question
+  def check(input)
+    if (input.to_i == self.answer)
+      puts "#{self.player.name}: YES! You are correct."
+    else
+      puts "#{self.player.name}: Seriously? No!"
+      self.player.lives -= 1
+    end
+  end
 end
